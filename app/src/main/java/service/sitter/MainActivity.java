ed6b0dc1e1b8639cbbfccc41d8a6f27f0d81dde7
danaadam <dana.adam.mail@gmail.com>
2021-09-04 16:41:18 +0300

    init

--- app/src/main/java/service/sitter/MainActivity.java
+++ app/src/main/java/service/sitter/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // bla
 //        FirebaseFirestore fireStore = FirebaseFirestore.getInstance();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
