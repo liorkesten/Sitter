e6cbf1ad9a5c8e92bfbe7d61727eeaad3ccd53ee
noamkesten <noamkesten@gmail.com>
2021-09-10 11:19:35 +0300

    [Home][2/n] added location

--- app/src/main/java/service/sitter/ui/fragments/LocationFragment.java
+++ app/src/main/java/service/sitter/ui/fragments/LocationFragment.java
@@ -2,32 +2,36 @@ package service.sitter.ui.fragments;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
+
+import java.util.Arrays;
 
 import service.sitter.R;
 
-public class LocationFragment extends Fragment implements OnMapReadyCallback {
-    private boolean locationPermissionGranted = false;
-    private MutableLiveData<Integer> locationLiveData;
+public class LocationFragment extends Fragment {
+    private MutableLiveData<String> locationLiveData;
+    private String location;
+    private AutocompleteSupportFragment autocompleteSupportFragment;
 
     public LocationFragment() {
         super(R.layout.fragment_location);
@@ -37,54 +41,37 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        Log.d("LocationFragment", "Creating");
         View view = inflater.inflate(R.layout.fragment_location, container, false);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        Log.d("LocationFragment", "map:" + mapFragment);
-//        mapFragment.getMapAsync(this);
-//
-//        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
-//                android.Manifest.permission.ACCESS_COARSE_LOCATION)
-//                == PackageManager.PERMISSION_GRANTED) {
-//            locationPermissionGranted = true;
-//        } else {
-//
-//            // You can directly ask for the permission.
-//            // The registered ActivityResultCallback gets the result of this request.
-//            requestPermissionLauncher.launch(
-//                    Manifest.permission.ACCESS_COARSE_LOCATION);
-//        }
+        // Initialize the AutocompleteSupportFragment.
+        String apikey = "AIzaSyAS0ehL2i_SFTq8GUhbVtshTa2oIK3p-P0";
+        Places.initialize(getContext(), apikey);
+        PlacesClient placesClient = Places.createClient(getContext());
+
+        autocompleteSupportFragment = (AutocompleteSupportFragment) getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+        autocompleteSupportFragment.setHint("Enter Location");
+        autocompleteSupportFragment.setCountry("ISR");
+        ((EditText) autocompleteSupportFragment.getView().findViewById(R.id.places_autocomplete_search_input)).setTextSize(20.0f);
+        autocompleteSupportFragment.getView().findViewById(R.id.places_autocomplete_search_button).setVisibility(View.GONE);
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG, Place.Field.ID, Place.Field.NAME));
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                location = place.getName();
+                locationLiveData.setValue(location);
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                Log.e(getTag(), "An error occured " + status);
+            }
+        });
+
 
         return view;
     }
 
-    public LiveData<Integer> getLiveData() {
+    public LiveData<String> getLiveData() {
         return locationLiveData;
     }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        Log.d("LocationFragment", "map starts");
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
-        Log.d("LocationFragment", "map ready");
-    }
-
-    private final ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    // Permission is granted. Continue the action or workflow in your
-                    // app.
-                    locationPermissionGranted = true;
-                } else {
-                    // Explain to the user that the feature is unavailable because the
-                    // features requires a permission that the user has denied. At the
-                    // same time, respect the user's decision. Don't link to system
-                    // settings in an effort to convince the user to change their
-                    // decision.
-                }
-            });
 }
