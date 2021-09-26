d031d4735daac7787704bf2e350515504f2fa782
t-liorkesten <t-liorkesten@microsoft.com>
2021-09-27 00:21:54 +0300

    Added cache for imageView

--- app/src/main/java/service/sitter/utils/ImagesUtils.java
+++ app/src/main/java/service/sitter/utils/ImagesUtils.java
@@ -1,12 +1,18 @@
 package service.sitter.utils;
 
+import android.net.Uri;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import service.sitter.db.DataBaseUtils;
 
 public class ImagesUtils {
+
+    public static Map<String, Uri> cachedUris = new HashMap<String, Uri>();
 
     /**
      * Gets imageUri and image view and upload the imageUri into the image view using Picasso.
@@ -15,7 +21,12 @@ public class ImagesUtils {
      * @param imageView
      */
     public static void updateImageView(String imageUri, ImageView imageView) {
-        DataBaseUtils.loadImage(imageUri, (uri) -> Picasso.get().load(uri).into(imageView));
-
+        if (!cachedUris.containsKey(imageUri)) {
+            DataBaseUtils.loadImage(imageUri, (uri) -> {
+                cachedUris.put(imageUri, uri);
+                Picasso.get().load(uri).into(imageView);
+            });
+        }
+        Picasso.get().load(cachedUris.get(imageUri)).into(imageView);
     }
 }
