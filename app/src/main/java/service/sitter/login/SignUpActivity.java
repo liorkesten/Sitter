e195ea3ea94935f34ea72bae1ab0d11e48ec4570
danaadam <dana.adam.mail@gmail.com>
2021-09-26 17:17:09 +0300

    backbutton

--- app/src/main/java/service/sitter/login/SignUpActivity.java
+++ app/src/main/java/service/sitter/login/SignUpActivity.java
@@ -2,16 +2,30 @@ package service.sitter.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import service.sitter.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private String firstName, lastName, mail, pass, rePass;
+    ImageButton backButton;
 
+    private String firstName = "", lastName = "", mail = "", pass = "", rePass = "";
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public boolean validate(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,34 +36,48 @@ public class SignUpActivity extends AppCompatActivity {
         EditText password = findViewById(R.id.et_password);
         EditText retypePassword = findViewById(R.id.et_repassword);
         Button registerButton = findViewById(R.id.btn_register);
+        backButton = findViewById(R.id.btn_back);
 
 
         registerButton.setOnClickListener(v -> {
+            boolean checkName = true, checkMail = true, checkPass = true;
             String name = fullName.getText().toString();
             String[] nameArr = name.split("\\s+");
-            if (nameArr.length != 2)
+            if (nameArr.length != 2) {
                 fullName.setError("You must enter first and last name to register.");
-            firstName = nameArr[0];
-            lastName = nameArr[1];
-            if (firstName.isEmpty() || lastName.isEmpty()){
+                checkName = false;
+            }
+            else{
+                firstName = nameArr[0];
+                lastName = nameArr[1];
+            }
+            if ((firstName.isEmpty() || lastName.isEmpty()) && checkName){
                 fullName.setError("You must enter a username to register.");
+                checkName = false;
             }
             mail = email.getText().toString();
             if (mail.isEmpty()){
                 email.setError("You must enter a mail address to register.");
+                checkMail = false;
+            }
+            if (!validate(mail) && checkMail){
+                email.setError("Invalid mail address");
             }
             pass = password.getText().toString();
             if(pass.isEmpty()){
                 password.setError("You must enter a password to register.");
+                checkPass = false;
             }
             rePass = retypePassword.getText().toString();
             if(rePass.isEmpty()){
                 retypePassword.setError("You must re-enter the password to register.");
+                checkPass = false;
             }
             if(!rePass.equals(pass)){
                 retypePassword.setError("Passwords must match.");
+                checkPass = false;
             }
-            else {
+            if (checkName && checkMail && checkPass){
                 Intent intentSetProfile = new Intent(SignUpActivity.this, SetProfile.class);
                 intentSetProfile.putExtra("firstName", firstName);
                 intentSetProfile.putExtra("lastName", lastName);
@@ -59,5 +87,7 @@ public class SignUpActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        backButton.setOnClickListener(v -> finish());
     }
 }
