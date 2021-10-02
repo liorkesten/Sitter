11c9fec5aa09455bea623c88022b46ed761eec9e
noamkesten <noamkesten@gmail.com>
2021-10-02 22:40:46 +0300

    UI Changes

--- app/src/main/java/service/sitter/ParentActivity.java
+++ app/src/main/java/service/sitter/ParentActivity.java
@@ -37,5 +37,13 @@ public class ParentActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
 
 }
